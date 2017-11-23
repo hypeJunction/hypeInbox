c62e1f34169e3fc52e95f4987bb6b3af41289c51
RiverVanRain <river.vanrain@gmail.com>
2017-11-23 20:18:57 +0300

    fix(menus): add missing title menu
    on 'Sent messages' page

--- views/default/resources/messages/sent.php
+++ views/default/resources/messages/sent.php
@@ -1,4 +1,5 @@
 <?php
+use hypeJunction\Inbox\Message;
 
 $page_owner = elgg_get_page_owner_entity();
 if (!$page_owner || !$page_owner->canEdit()) {
@@ -9,6 +10,19 @@ elgg_require_js('framework/inbox/user');
 
 elgg_push_breadcrumb(elgg_echo('inbox'), "messages/inbox/$page_owner->username");
 elgg_push_breadcrumb(elgg_echo('inbox:sent'), "messages/outbox/$page_owner->username");
+
+$outgoing_message_types = hypeInbox()->model->getOutgoingMessageTypes();
+foreach ($outgoing_message_types as $mt) {
+	elgg_register_menu_item('title', array(
+		'name' => ($mt == HYPEINBOX_PRIVATE) ? "send" : "compose:$mt",
+		'text' => elgg_echo('inbox:new', array(elgg_echo("item:object:message:$mt:singular"))),
+		'href' => elgg_http_add_url_query_elements('messages/compose', array(
+			'message_type' => $mt,
+			'send_to' => get_input('send_to', null),
+		)),
+		'link_class' => 'elgg-button elgg-button-action',
+	));
+}
 
 $params = array(
 	'filter_context' => 'sent',
